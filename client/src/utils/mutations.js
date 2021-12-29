7f4da9c088d34086f877febbc652dcb6b73eeb78
Thorson <thorsonb@nala.roche.com>
2021-12-29 09:54:05 -0800

    add mutations and queries.js updated API.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,68 @@
+import gql from "graphql-tag";
+
+// Create a GraphQL mutation query and adds a new user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+// Creates a GraphQL mutation saveBook query and adds a new book to the database
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: savedBook!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        bookId
+        authors
+        link
+        description
+        bookId
+        image
+        title
+      }
+    }
+  }
+`;
+
+// Creates a GraphQL mutation removeBook query and deletes a book from the database
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
