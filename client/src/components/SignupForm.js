ef3c7d618493c9f2d93d974bb3ebbd753393dc05
Thorson <thorsonb@nala.roche.com>
2021-12-29 11:11:47 -0800

    refactor SavedBooks.js for apollo server and graphql

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -54,9 +54,7 @@ const SignupForm = () => {
 
   return (
     <>
-      {/* This is needed for the validation functionality above */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
         <Alert
           dismissible
           onClose={() => setShowAlert(false)}
